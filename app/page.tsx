7bb7c97bb2c02ddc4c1e77af776fd7857332809f
suming82 <suming82@yahoo.co.uk>
2025-09-08 17:53:17 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "../lib/supabaseClient";
 import { Auth, ThemeMinimal } from "@supabase/auth-ui-react";
 
 function PdfUploader() {
