440b19c9557d96c2002d6c2007b3b3d1cb38274f
suming82 <suming82@yahoo.co.uk>
2025-09-08 22:25:01 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,2 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: true
-  }
-};
-
+const nextConfig = { reactStrictMode: true, typescript: { ignoreBuildErrors: true } };
 module.exports = nextConfig;
